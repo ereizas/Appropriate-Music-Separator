bb40e66a830930617cc0c6bc6ec08cff8b1c1271
Eric Reizas <ereizas@gmail.com>
2022-10-29 16:13:58 -0400

    Added parsing of Spotify link to find playlist ID

--- MusicSources.py
+++ MusicSources.py
@@ -1,8 +1,14 @@
 import requests
 
-
 def spotify(link):
-    pass
+    playlistID = ''
+    if('?' in link):
+        playlistID = link[link.index('playlist/')+9:link.index('?')]
+    else:
+        playlistID = link[link.index('playlist/')+9:]
+    
+
+print(spotify('https://open.spotify.com/playlist/31hXsTQWKdum0YD6eHzLGf?si=yCdM_Gg_S6yI10rHweJl3Q'))
 
 def appleMusic(link):
     pass
