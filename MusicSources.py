2e4b39662c4c430c70963727146e10d19b1b9956
Eric Reizas <ereizas@gmail.com>
2022-10-29 15:44:03 -0400

    Added link parameter to MusicSources Functions

---
+++ MusicSources.py
@@ -0,0 +1,17 @@
+def spotify(link):
+    pass
+
+def appleMusic(link):
+    pass
+
+def youtube(link):
+    pass
+
+def soundcloud(link):
+    pass
+
+def folder(link):
+    pass
+
+def m3u(link):
+    pass
