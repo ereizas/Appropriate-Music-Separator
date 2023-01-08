ccd9cb827b7c8e4dc333ce2f515724581dbb95d0
Eric Reizas <ereizas@gmail.com>
2023-01-08 13:27:05 -0500

    Completed bit string conversion and padding steps of encryption

--- Encryption.py
+++ Encryption.py
@@ -1,2 +1,15 @@
+#sha256 encryption is used
 def encrypt(string):
-    pass
+    bitString = ''
+    for char in string:
+        bitString+=bin(ord(char)).replace('0b','')
+    originalBitStr = bin(len(bitString)).replace('0b','')
+    lenOfBitStrofLenOfOriginalBitStr = len(originalBitStr)
+    while len(bitString)%512!=512-lenOfBitStrofLenOfOriginalBitStr:
+        bitString+='0'
+    bitString+=originalBitStr
+    
+
+
+
+#print(encrypt("hello world"))
