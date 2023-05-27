9cdc8d0517c47a10b398fb994ffbc940ec7f6bc4
Eric Reizas <ereizas@gmail.com>
2023-05-27 15:27:22 -0400

    Began API access of lyrics

---
+++ LyricParsing.py
@@ -0,0 +1,39 @@
+import requests, config
+
+#these functions return a boolean value: true for appropriate, false for not
+
+def lyristLyrics(strArtists,songTitleFormatted):
+    response = requests.get('https://lyrist.vercel.app/api/' + songTitleFormatted + '/' + strArtists)
+    lyrics = ''
+    data = response.json()
+    if(response.status_code==200):
+        lyrics=data['lyrics']
+
+def geniusLyrics(artists,strArtists, songTitle, songTitleFormatted):
+    clientID, clientSecret = config.geniusClientID,config.geniusClientSecret
+    authResponse = requests.post('https://api.genius.com/oauth/token', {
+    'grant_type': 'client_credentials',
+    'client_id': clientID,
+    'client_secret': clientSecret,
+    })
+    authResponseData = authResponse.json()
+    accessToken = authResponseData['access_token']
+    geniusReqHeaders = {'Authorization': 'Bearer {token}'.format(token=accessToken)}
+    geniusSearchResponse = requests.get("https://api.genius.com/search?q="+ '%20'.join(strArtists)+'%20'+songTitleFormatted,headers=geniusReqHeaders)
+    geniusSearchData = geniusSearchResponse.json()
+    print(' '.join(artists) + '-' + songTitle + '\n')
+    for hit in geniusSearchData['response']['hits']:
+        artistsAllPresent = True
+        for artist in artists:
+            if artist.lower() not in hit['result']['artist_names'].lower():
+                print(hit['result']['id'])
+                print(hit['result']['artist_names'].lower()+'\n')
+                artistsAllPresent=False
+                break
+        if not artistsAllPresent:
+            continue
+        elif hit['result']['language'] == 'en' and songTitle in hit['result']['full_title']:
+            songResponse = requests.get('https://api.genius.com/songs/' + str(hit['result']['id']) + '?text_format=plain',headers=geniusReqHeaders)
+            songData = songResponse.json()
+            print(songData['response']['song']['embed_content'] + '\n')
+            break
