bba76592216ed906c8c053cb48745b4c498c0209
Eric Reizas <ereizas@gmail.com>
2023-06-17 13:05:36 -0400

    Added link validation and documentation for code

--- PlaylistParsing.py
+++ PlaylistParsing.py
@@ -1,4 +1,4 @@
-import requests, LyricParsing, config
+import LyricParsing, config
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import google_auth_oauthlib.flow
@@ -6,13 +6,15 @@ import googleapiclient.discovery
 import googleapiclient.errors
 #Test invalid links given
 
-def getAppropSpotifySongs(link: str)->list[str]:
+def getAppropSpotifySongs(link:str)->list[str]:
 	"""
 	This function is meant to parse the Spotfiy playlist data to retrieve the Spotify ids for the appropriate songs and update and return an array with those ids.
-	
 	@param link : link to playlist
 	@return appropSongIds : list with string Spotify ids of the appropriate songs in the playlist
 	"""
+	
+	if 'spotify' not in link:
+		return 'Not a valid Spotify link'
 	playlistID, appropSongIds= '', []
 	token = SpotifyOAuth(client_id=config.spotifyClientID,client_secret=config.spotifyClientSecret,scope="playlist-read-private",redirect_uri="https://localhost:8080/callback")
 	spotifyObj = spotipy.Spotify(auth_manager=token)
@@ -21,16 +23,14 @@ def getAppropSpotifySongs(link: str)->list[str]:
 		playlistID = link[link.index('playlist/')+9:link.index('?')]
 	else:
 		playlistID = link[link.index('playlist/')+9:]
-	#response = requests.get('https://api.spotify.com/v1/playlists/'+playlistID+'/tracks',headers=headers)
 	data = spotifyObj.playlist_tracks(playlistID,fields=any)
-	moreSongsLeft = True
-	#var used for reattempting get request for next data for a playlist
-	nextTempLink = ''
-	while(moreSongsLeft):
-		#reattempting request if data does not have required info
+	#while there are more songs in the playlist to get data from
+	while(data['next']):
+		#refreshes access token if it has expired
 		if 'error' in data and data['error']['status']==401:
 			token = SpotifyOAuth(client_id=config.spotifyClientID,client_secret=config.spotifyClientSecret,scope="playlist-read-private",redirect_uri="https://localhost:8080/callback")
 			spotifyObj = spotipy.Spotify(auth_manager=token)
+		#for other errors, retries three times
 		elif 'items' not in data:
 			attempts = 0
 			while attempts<3 and 'items' not in data:
@@ -42,23 +42,29 @@ def getAppropSpotifySongs(link: str)->list[str]:
 				if not item['track']['explicit']:
 					artists, songTitle = [], item['track']['name']
 					for artist in item['track']['artists']:
+						#prevents featured artists mentioned in song title from being repeated
 						if(artist['name'] not in songTitle):
 							artists.append(artist['name'])
 					#passing artists into an array enforces the perception of artists as being an array
-					LyricParsing.findAndParseLyrics([artists],songTitle,appropSongIds,item['track']['id'])
-			if(data['next']!=None):
-				nextTempLink = data['next']
-				#check if this works
-				data= spotifyObj.next(data) #playlist_tracks(playlistID,fields=any)
-			else:
-				moreSongsLeft=False
-		except:
-			print(data)
+					LyricParsing.findAndParseLyrics(artists,songTitle,appropSongIds,item['track']['id'],'')
+			data = spotifyObj.next(data)
+		except Exception as e:
+			print(e)
+			break
 	return appropSongIds
 
 #try to find lyrics first and then YT transcript as last resort
-def getAppropYTSongs(link):
-	#given a playlist link from either a specific video on the list (which means extra data in the link) or just the playlist itself, lastAmpInd will be the needed end index for playlistID
+def getAppropYTSongs(link:str)->list[str]:
+	"""
+	This function parses the songs in a YouTube playlist and returns the ids of those songs that are appropriate for children.
+	@param link : link to YouTube playlist
+	@return youtube : resource object with necessary credentials stored to read, create and update playlists
+	@return appropSongIDs : list of YouTube ids for videos deemed appropriate for children by the program
+	"""
+
+	if 'youtube' not in link:
+		return 'Not a valid YouTube link'
+	#given a playlist link from either a specific video on the list (which means the video id is in the playlist link) or just the playlist itself, lastAmpInd will be the needed end index for playlistID
 	lastAmpInd = link.rfind('&')
 	listEqInd = link.find('list=')
 	if(lastAmpInd<listEqInd):
@@ -66,10 +72,11 @@ def getAppropYTSongs(link):
 	playlistID = link[link.index('list=')+5:lastAmpInd]
 	#Must copy file name into first parameter
 	flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-	'client_secret.apps.googleusercontent.com.json', scopes=['https://www.googleapis.com/auth/youtube.readonly','https://www.googleapis.com/auth/youtube.force-ssl'])
+	config.googleClientSecretFileName, scopes=['https://www.googleapis.com/auth/youtube.readonly','https://www.googleapis.com/auth/youtube.force-ssl'])
 	credentials = flow.run_local_server()
 	youtube = googleapiclient.discovery.build('youtube', 'v3', credentials=credentials)
 	appropSongIDs = []
+	#initialized to 50 to allow the loop to run, reassigned later
 	numResults = 50
 	nextPageToken = ''
 	#While the results returned by the current request is greater than or equal to 50 (the max number of results that can be returned), the program will keep requesting transcripts
@@ -97,11 +104,12 @@ def getAppropYTSongs(link):
 				songTitle = vidTitle[vidTitle.find(' - ')+3:endIndForSongTitleStr-1]
 				print(artists + ' - ' + songTitle)
 			else:
+				#if video title is not in standard format of "Artist Name - Song Title", then default to info below which should be more favorable for api searches
 				songTitle = vidTitle
 			LyricParsing.findAndParseLyrics([artists],songTitle,appropSongIDs,item['snippet']['resourceId']['videoId'],youtube)
 		if 'nextPageToken' in response:
 			nextPageToken=response['nextPageToken']
-	return [youtube,appropSongIDs]
+	return youtube,appropSongIDs
 		
 def getAppropYTMusicSongs(link):
 	pass
