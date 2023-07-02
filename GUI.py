ed3f3ac8b177dd5d3d2ab5a5b92ed08a6e9fb719
Eric Reizas <ereizas@gmail.com>
2023-07-02 12:41:38 -0400

    Integrated Spotify parsing backend code with GUI

--- GUI.py
+++ GUI.py
@@ -17,12 +17,11 @@ class GUI():
         
         generalFrame=ttk.Frame(self.__root)
         generalFrame.pack(padx=5,pady=5)
-        self.prevRunAppropYTIDs = StringVar()
-        self.originalLink = StringVar()
+        self.prevRunAppropIDs = StringVar()
         self.newLink = StringVar()
         self.playlistTitle = StringVar()
         self.descrip = StringVar()
-        self.buildGeneralGUI(generalFrame,self.prevRunAppropYTIDs,self.originalLink,self.newLink,self.playlistTitle,self.descrip)
+        self.buildGeneralGUI(generalFrame,self.prevRunAppropIDs,self.newLink,self.playlistTitle,self.descrip)
 
         ytOptionsFrame = ttk.Frame(self.__root)
         ytOptionsFrame.pack(padx=5,pady=5)
@@ -38,27 +37,27 @@ class GUI():
         endFrame.pack(padx=5,pady=5)
         self.buildEndFrame(endFrame)
 
-    def buildGeneralGUI(self,master,prevRunAppropYTIDs,originalLink,newLink,playlistTitle,descrip):
+    def buildGeneralGUI(self,master,prevRunAppropIDs,newLink,playlistTitle,descrip):
         """
         Builds the part of the GUI that applies for all types of playlists
         """
-        ttk.Label(master,text='If you have it, paste the list of YouTube IDs from a previous run:').grid(row=0,column=0)
-        self.prevRunAppropYTIDsEntry = ttk.Label(master,width=100,textvariable=prevRunAppropYTIDs)
-        self.prevRunAppropYTIDsEntry.grid(row=1,column=0)
+        ttk.Label(master,text='If you have it, paste the list of appropriate song IDs from a previous run:').grid(row=0,column=0)
+        self.prevRunAppropIDsEntry = ttk.Entry(master,width=100,textvariable=prevRunAppropIDs)
+        self.prevRunAppropIDsEntry.grid(row=1,column=0)
         ttk.Label(master,text='Enter or paste in the link of the playlist you want the program to analyze:').grid(row=2,column=0)
-        self.linkEntry = ttk.Entry(master,width=50,textvariable=originalLink)
+        self.linkEntry = ttk.Entry(master,width=100)
         self.linkEntry.grid(row=3,column=0)
         #for spacing
         ttk.Frame(master).grid(row=4,column=0,pady=5)
         ttk.Label(master,text='Select the streaming service of the playlist:').grid(row=5,column=0)
-        streamingServiceDropDown = ttk.Combobox(master,values=['Spotify','YouTube Music','YouTube'])
-        streamingServiceDropDown.grid(row=6,column=0)
-        ttk.Label(master,text='Enter the link for the playlist you want the appropriate songs to be added to (recommended for YouTube, not as important for YouTube music) or skip to the next entry to create a new one:').grid(row=7,column=0)
-        self.newPlaylistLinkEntry = ttk.Entry(master,width=50,textvariable=newLink)
-        self.newPlaylistLinkEntry.grid(row=8,column=0)
+        self.streamingServiceDropDown = ttk.Combobox(master,values=['Spotify','YouTube Music','YouTube'])
+        self.streamingServiceDropDown.grid(row=6,column=0)
+        ttk.Label(master,text='Enter the link for a premade playlist(optional, but highly recommended for YouTube, and for Spotify the playlist MUST be private) or skip to the next entry to create a new one:').grid(row=7,column=0)
+        self.premadePlaylistLinkEntry = ttk.Entry(master,width=100,textvariable=newLink)
+        self.premadePlaylistLinkEntry.grid(row=8,column=0)
         ttk.Label(master,text='Enter a title for the new playlist:').grid(row=9,column=0)
-        self.TitleEntry = ttk.Entry(master,width=40,textvariable=playlistTitle)
-        self.TitleEntry.grid(row=10,column=0)
+        self.titleEntry = ttk.Entry(master,width=40,textvariable=playlistTitle)
+        self.titleEntry.grid(row=10,column=0)
         ttk.Label(master,text="Enter a description for the new playlist (optional):").grid(row=11,column=0)
         self.descripEntry = ttk.Entry(master,width=50,textvariable=descrip)
         self.descripEntry.grid(row=12,column=0)
@@ -80,13 +79,36 @@ class GUI():
         self.ytVidsQuotaWaitCheckbutton.grid(row=4,column=0,pady=3)
     
     def separatePlaylist(self):
-        print("test")
+        #clears the output text box
+        if self.outputTextBox.get('1.0',END):
+            self.outputTextBox.delete('1.0',END)
+        self.outputTextBox.insert(END,"Processing playlist, make sure to check the command line/terminal (For Windows: PowerShell) that you ran this on for any updates.\n")
+        streamingService = self.streamingServiceDropDown.get()
+        if streamingService=='Spotify':
+            spotifyGetRet = PlaylistParsing.getAppropSpotifySongs(self.linkEntry.get())
+            if type(spotifyGetRet)==str:
+                self.outputTextBox.insert(END,str(spotifyGetRet)+'\n')
+            else:
+                message, strAppropSongIDs, newLink = PlaylistCreation.createSpotifyPlaylist(self.premadePlaylistLinkEntry.get(),self.titleEntry.get(),self.descripEntry.get(),spotifyGetRet,config.spotifyUserID)
+                if 'Error:' in message:
+                    outputTBMessage = message + " Appropriate Song IDs: " + strAppropSongIDs
+                    if newLink:
+                        outputTBMessage+=newLink
+                    self.outputTextBox.insert(END,outputTBMessage)
+                else:
+                    self.outputTextBox.insert(END,message)
+        elif streamingService=='YouTube Music':
+            pass
+        elif streamingService=='YouTube':
+            pass
+        else:
+            self.outputTextBox.insert(END,'The streaming service entered is either not available or not valid.')
     
     def buildEndFrame(self,master):
         self.separateButton = ttk.Button(master,text='Separate Appropriate Music',command=self.separatePlaylist)
         self.separateButton.grid(row=0,column=0)   
         ttk.Label(master,text='Output:').grid(row=1,column=0)
-        self.outputTextBox = ttk.Entry(master)
+        self.outputTextBox = Text(master,width=150,height=15)
         self.outputTextBox.grid(row=2,column=0)
 
 if __name__=='__main__':
